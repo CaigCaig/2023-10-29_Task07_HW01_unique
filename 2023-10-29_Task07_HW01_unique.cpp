f287105a5ebe062f1778c58205d9972858ed93b4
CaigCaig <caig@inbox.ru>
2023-10-29 19:39:14 +0300

    Initial commit

---
+++ 2023-10-29_Task07_HW01_unique.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> v = { 1, 1, 2, 5, 6, 1, 2, 4 };
+    vector<int>::iterator it;
+    cout << "[IN]: ";
+    for (int i = 0; i < v.size(); i++) cout << " " << v[i];
+    cout << endl;
+
+    sort(v.begin(), v.end());
+    it = unique(v.begin(), v.end());
+    v.resize(std::distance(v.begin(), it));
+
+    cout << "[OUT]: ";
+    for (int i = 0; i < v.size(); i++) cout << " " << v[i];
+    cout << endl;
+
+    system("pause");
+}
